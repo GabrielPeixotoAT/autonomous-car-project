7e7dc0ddb8ba7c03ba2badc1085e645279ce1dcc
Gabriel Peixoto <gabr.art.design@gmail.com>
2022-09-23 21:47:01 -0300

    feat: começo da implementação do carro

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -4,15 +4,95 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] wheels;
+    public WheelCollider[] colliders;
+    
+    float inputView;
+
+    public float torque;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        SetWheelsPosition();
+
+        inputView = Input.GetAxis("Vertical");
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            FreeBrakeAll();
+            AccelerateMotor(0,1);
+            AccelerateMotor(2,3);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            FreeBrakeAll();
+            AccelerateMotorReverse(0,1);
+            AccelerateMotor(2,3);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            FreeBrakeAll();
+            AccelerateMotorReverse(2,3);
+            AccelerateMotor(0,1);
+        }
+        if (false)
+        {
+            StopCar();
+        }
+    }
+
+    void StopCar()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            colliders[i].brakeTorque = torque;
+        }
+    }
+
+    void AccelerateMotor(int wheel1, int wheel2)
+    {
+        colliders[wheel1].motorTorque = torque * (-1);
+        colliders[wheel2].motorTorque = torque * (-1);
+    }
+
+    void AccelerateMotorReverse(int wheel1, int wheel2)
+    {
+        colliders[wheel1].motorTorque = torque;
+        colliders[wheel2].motorTorque = torque;
+    }
+
+
+    void BrakeMotor(int wheel1, int wheel2)
+    {
+        colliders[wheel1].brakeTorque = torque/2;
+        colliders[wheel2].brakeTorque = torque/2;
+    }
+
+    void FreeBrake(int wheel1, int wheel2)
+    {
+        colliders[wheel1].brakeTorque = 0;
+        colliders[wheel2].brakeTorque = 0;
+    }
+
+    void FreeBrakeAll()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            colliders[i].brakeTorque = 0;
+        }
+    }
+
+    void SetWheelsPosition()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            wheels[i].transform.position = colliders[i].transform.position;
+            wheels[i].transform.rotation = colliders[i].transform.rotation;
+        }
     }
 }
