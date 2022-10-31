27be142f515efabc926ea38808e76df3ca272a30
Gabriel Peixoto <gabr.art.design@gmail.com>
2022-10-30 21:04:34 -0300

    fix: identificar resolução atual no menu settings

--- Assets/Scripts/UI/Menus/SettingsMenu.cs
+++ Assets/Scripts/UI/Menus/SettingsMenu.cs
@@ -1,10 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     bool fullScreen = true;
+    
+    int current;
+
+    public Dropdown resolutionInput, fullScreenInput, qualityInput;
+
+    void Start()
+    {
+        current = Screen.width;
+        
+        switch (current)
+        {
+            case 800:
+                resolutionInput.value = 0;
+                break;
+            case 1280:
+                resolutionInput.value = 1;
+                break;
+            case 1366:
+                resolutionInput.value = 2;
+                break;
+            case 1920:
+                resolutionInput.value = 3;
+                break;
+        }
+    }
 
     public void SetScreenResolution(int index)
     {
